ea4e43af5ce428fe2ff6fc1858b424553faa60d4
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2024-01-03 16:48:27 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { NavigationLink, Button, FlowerBox } from "../components";
+import { NavigationLink, Button } from "../components";
 import Image from 'next/image';
 
 import styles from "./page.module.scss";
 import coco1 from "../../assets/coco1.jpg";
 import coco2 from "../../assets/coco2.jpg";
+import FlowerBox from "../components/FlowerBox";
 
 
 const navigationLinks = [
