00a6a7ee6611bd8c566187e0daeb040a668fb6dd
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2024-01-23 22:39:47 -0300

    cambios para netlify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,39 @@ import styles from "./page.module.scss";
 import logo from "../../assets/logoONG.png";
 import { useEffect, useRef, useState } from "react";
 import { useSpring, animated } from "react-spring";
-import { flowerData, navigationLinks } from "./data";
 import { Footer } from "../components/footer/footer";
+import coco1 from "../../assets/coco1.jpg";
+import coco2 from "../../assets/coco2.jpg";
 
+export const flowerData = [
+  {
+    id: 'coco1',
+    image: coco1,
+    alt: "Dairy Queen",
+    text: "Dairy Queen (DQ) - Texto descriptivo",
+  },
+  { 
+    id: 'coco2',image: coco2, alt: "Tel Aviv", text: "Tel Aviv - Texto descriptivo" },
+  {    id: 'coco3',
+    image: coco1,
+    alt: "Dairy Queen",
+    text: "Dairy Queen (DQ) - Texto descriptivo",
+  },
+  {     id: 'coco4',image: coco2, alt: "Tel Aviv", text: "Tel Aviv - Texto descriptivo" },
+  {
+    id: 'coco5',image: coco1,
+    alt: "Dairy Queen",
+    text: "Dairy Queen (DQ) - Texto descriptivo",
+  },
+  {    id: 'coco6', image: coco2, alt: "Tel Aviv", text: "Tel Aviv - Texto descriptivo" },
+  {
+    id: 'coco7',
+    image: coco1,
+    alt: "Dairy Queen",
+    text: "Dairy Queen (DQ) - Texto descriptivo",
+  },
+
+];
 export default function Page() {
   const [showText, setShowText] = useState(false);
 
